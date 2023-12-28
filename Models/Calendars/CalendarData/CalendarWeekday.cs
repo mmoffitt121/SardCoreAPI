d972ed902bc472d41fbdf34797bd83a88b74c208
Matthew Moffitt <mmoffitt121@gmail.com>
2023-12-28 15:22:36 -0600

    Bugfix

--- Models/Calendars/CalendarData/CalendarWeekday.cs
+++ Models/Calendars/CalendarData/CalendarWeekday.cs
@@ -5,8 +5,8 @@ namespace SardCoreAPI.Models.Calendars.CalendarData
     public class CalendarWeekday : IValidatable
     {
         public int Sequence { get; set; }
-        public int Name { get; set; }
-        public int Summary { get; set; }
+        public string Name { get; set; }
+        public string? Summary { get; set; }
         public char Formatter { get; set; }
 
         public List<string> Validate()
