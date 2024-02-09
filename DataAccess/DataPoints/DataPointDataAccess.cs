02e13599a55afddcc4cbb085b5a3744e3a0e4fb3
Matthew Moffitt <mmoffitt121@gmail.com>
2024-02-08 22:42:36 -0600

    Root out search, change approach, broken bulid

--- DataAccess/DataPoints/DataPointDataAccess.cs
+++ DataAccess/DataPoints/DataPointDataAccess.cs
@@ -6,6 +6,7 @@ using SardCoreAPI.Models.DataPoints;
 using SardCoreAPI.Models.DataPoints.DataPointParameters;
 using SardCoreAPI.Models.Hub.Worlds;
 using System.Drawing.Printing;
+using System.Dynamic;
 using System.Runtime.Intrinsics.Arm;
 
 namespace SardCoreAPI.DataAccess.DataPoints
@@ -21,7 +22,6 @@ namespace SardCoreAPI.DataAccess.DataPoints
             }
 
             string sql = $@"SELECT * FROM DataPoints dp
-                    /**leftjoin**/
                     /**where**/
                     ORDER BY Name
                     {pageSettings}
@@ -29,48 +29,18 @@ namespace SardCoreAPI.DataAccess.DataPoints
 
             SqlBuilder builder = new SqlBuilder();
             var template = builder.AddTemplate(sql);
+            dynamic valueBank = BuildValueBank(criteria);
 
-            if (criteria.Parameters != null && criteria.Parameters.Count() > 0)
+            if (criteria.Parameters != null && criteria.Parameters.Count() > 0 && criteria.ParameterSearchOptions != null && criteria.ParameterSearchOptions.Count() > 0)
             {
-                for (int i = 0; i < Math.Min(criteria.Parameters.Count(), 20); i++)
+                int i = 0;
+                criteria.ParameterSearchOptions.ForEach(opt =>
                 {
-                    if (criteria.Parameters[i].GetType() == typeof(DataPointParameterBoolean))
-                    {
-                        criteria.SetParam(i, ((DataPointParameterBoolean)criteria.Parameters[i]).BoolValue ? "1" : "0");
-                        builder.LeftJoin($"DataPointParameterBoolean p{i} ON p{i}.DataPointId = dp.Id");
-                        builder.Where($"p{i}.Value = @Param{i}");
-                    }
-                    if (criteria.Parameters[i].GetType() == typeof(DataPointParameterDataPoint))
-                    {
-                        criteria.SetParam(i, ((DataPointParameterDataPoint)criteria.Parameters[i]).DataPointValueId.ToString());
-                        builder.LeftJoin($"DataPointParameterDataPoint p{i} ON p{i}.DataPointId = dp.Id");
-                        builder.Where($"p{i}.Value = @Param{i}");
-                    }
-                    if (criteria.Parameters[i].GetType() == typeof(DataPointParameterDouble) && ((DataPointParameterDouble)criteria.Parameters[i]).DoubleValue != null)
-                    {
-                        criteria.SetParam(i, ((DataPointParameterDouble)criteria.Parameters[i]).DoubleValue?.ToString() ?? "");
-                        builder.LeftJoin($"DataPointParameterDouble p{i} ON p{i}.DataPointId = dp.Id");
-                        builder.Where($"p{i}.Value = @Param{i}");
-                    }
-                    if (criteria.Parameters[i].GetType() == typeof(DataPointParameterInt) && ((DataPointParameterInt)criteria.Parameters[i]).IntValue != null)
-                    {
-                        criteria.SetParam(i, ((DataPointParameterInt)criteria.Parameters[i]).IntValue?.ToString() ?? "");
-                        builder.LeftJoin($"DataPointParameterInt p{i} ON p{i}.DataPointId = dp.Id");
-                        builder.Where($"p{i}.Value = @Param{i}");
-                    }
-                    if (criteria.Parameters[i].GetType() == typeof(DataPointParameterString) && !((DataPointParameterString)criteria.Parameters[i]).StringValue.IsNullOrEmpty())
-                    {
-                        criteria.SetParam(i, "%" + ((DataPointParameterString)criteria.Parameters[i]).StringValue + "%");
-                        builder.LeftJoin($"DataPointParameterString p{i} ON p{i}.DataPointId = dp.Id");
-                        builder.Where($"p{i}.Value LIKE @Param{i}");
-                    }
-                    if (criteria.Parameters[i].GetType() == typeof(DataPointParameterSummary) && !((DataPointParameterSummary)criteria.Parameters[i]).SummaryValue.IsNullOrEmpty())
-                    {
-                        criteria.SetParam(i, "%" + ((DataPointParameterSummary)criteria.Parameters[i]).SummaryValue + "%");
-                        builder.LeftJoin($"DataPointParameterSummary p{i} ON p{i}.DataPointId = dp.Id");
-                        builder.Where($"p{i}.Value LIKE @Param{i}");
-                    }
-                }
+                    var param = criteria.Parameters.FirstOrDefault(p => p.DataPointTypeParameterId == opt.DataPointTypeParameterId);
+                    opt.SequenceId = i;
+                    builder.Where($"dp.Id IN ({GetParameterSubquery(param, opt, valueBank)})");
+                    i++;
+                });
             }
 
             if (!string.IsNullOrEmpty(criteria.Query)) { builder.Where("dp.Name LIKE CONCAT('%', IFNULL(@Query, ''), '%')"); }
@@ -82,7 +52,107 @@ namespace SardCoreAPI.DataAccess.DataPoints
                 builder.Where("dp.TypeId IN @TypeIds");
             }
 
-            return await Query<DataPoint>(template.RawSql, criteria, info);
+            return await Query<DataPoint>(template.RawSql, valueBank, info);
+        }
+
+        private ExpandoObject BuildValueBank(DataPointSearchCriteria criteria)
+        {
+            dynamic valueBank = new ExpandoObject();
+            valueBank.TypeId = criteria.TypeId;
+            valueBank.TypeIds = criteria.TypeIds;
+            valueBank.Id = criteria.Id;
+            valueBank.Query = criteria.Query;
+            valueBank.PageNumber = criteria.PageNumber;
+            valueBank.PageSize = criteria.PageSize;
+
+            return valueBank;
+        }
+
+        private string GetParameterSubquery(DataPointParameter param, ParameterSearchOptions searchOptions, ExpandoObject valueBank)
+        {
+            string sql = $"SELECT DataPointId FROM {GetTableFromParam(param)} /**where**/";
+
+            SqlBuilder builder = new SqlBuilder();
+            var template = builder.AddTemplate(sql);
+
+            switch (searchOptions.FilterMode)
+            {
+                case ParameterSearchOptions.FilterModeEnum.Equals:
+                    string equals = $"Parameter{searchOptions.SequenceId}";
+                    builder.Where($"Value = {equals}");
+                    AddParamToBank(valueBank, equals, param);
+                    break;
+                case ParameterSearchOptions.FilterModeEnum.Contains:
+                case ParameterSearchOptions.FilterModeEnum.StartsWith:
+                case ParameterSearchOptions.FilterModeEnum.EndsWith:
+                    string like = $"Parameter{searchOptions.SequenceId}";
+                    bool startWildcard = searchOptions.FilterMode == ParameterSearchOptions.FilterModeEnum.Contains || searchOptions.FilterMode == ParameterSearchOptions.FilterModeEnum.StartsWith;
+                    bool endWildcard = searchOptions.FilterMode == ParameterSearchOptions.FilterModeEnum.Contains || searchOptions.FilterMode == ParameterSearchOptions.FilterModeEnum.EndsWith;
+                    builder.Where($"Value LIKE {like}");
+                    AddParamToBank(valueBank, like, param, startWildcard: startWildcard, endWildcard: endWildcard);
+                    break;
+                case ParameterSearchOptions.FilterModeEnum.GreaterThan:
+                    string greaterThan = $"Parameter{searchOptions.SequenceId}";
+                    builder.Where($"Value > {greaterThan}");
+                    AddParamToBank(valueBank, greaterThan, param);
+                    break;
+                case ParameterSearchOptions.FilterModeEnum.LessThan:
+                    string lessThan = $"Parameter{searchOptions.SequenceId}";
+                    builder.Where($"Value < {lessThan}");
+                    AddParamToBank(valueBank, lessThan, param);
+                    break;
+            }
+            builder.Where("Value = ");
+
+            valueBank.TryAdd($"Parameter{searchOptions.DataPointTypeParameterId}.{searchOptions.FilterMode}", "");
+
+            return template.RawSql;
+        }
+
+        private string GetTableFromParam(DataPointParameter param)
+        {
+            if (param.GetType() == typeof(DataPointParameterBoolean)) { return "DataPointParameterBoolean"; }
+            if (param.GetType() == typeof(DataPointParameterDataPoint)) { return "DataPointParameterDataPoint"; }
+            if (param.GetType() == typeof(DataPointParameterDouble)) { return "DataPointParameterDouble"; }
+            if (param.GetType() == typeof(DataPointParameterInt)) { return "DataPointParameterInt"; }
+            if (param.GetType() == typeof(DataPointParameterString)) { return "DataPointParameterString"; }
+            if (param.GetType() == typeof(DataPointParameterSummary)) { return "DataPointParameterSummary"; }
+
+            return "DataPointParameterString";
+        }
+
+        private void AddParamToBank(ExpandoObject valueBank, string key, DataPointParameter param, bool startWildcard = false, bool endWildcard = false)
+        {
+            if (param.GetType() == typeof(DataPointParameterBoolean)) valueBank.TryAdd(key, ((DataPointParameterBoolean)param).BoolValue);
+            if (param.GetType() == typeof(DataPointParameterDataPoint)) valueBank.TryAdd(key, ((DataPointParameterDataPoint)param).DataPointId);
+            if (param.GetType() == typeof(DataPointParameterDouble)) valueBank.TryAdd(key, ((DataPointParameterDouble)param).DoubleValue);
+            if (param.GetType() == typeof(DataPointParameterInt)) valueBank.TryAdd(key, ((DataPointParameterInt)param).IntValue);
+            if (param.GetType() == typeof(DataPointParameterString)) 
+            {
+                string val = ((DataPointParameterString)param).StringValue;
+                if (startWildcard) val = "%" + val;
+                if (endWildcard) val = val + "%";
+                valueBank.TryAdd(key, val); 
+            }
+            if (param.GetType() == typeof(DataPointParameterSummary)) 
+            {
+                string val = ((DataPointParameterSummary)param).SummaryValue;
+                if (startWildcard) val = "%" + val;
+                if (endWildcard) val = val + "%";
+                valueBank.TryAdd(key, val);
+            }
+        }
+
+        private string ExtractValueFromParameter(DataPointParameter param, ExpandoObject valueBank)
+        {
+            if (param.GetType() == typeof(DataPointParameterBoolean)) { return ((DataPointParameterBoolean)param).BoolValue; }
+            if (param.GetType() == typeof(DataPointParameterDataPoint)) { return ((DataPointParameterDataPoint)param).DataPointId; }
+            if (param.GetType() == typeof(DataPointParameterDouble)) { return ((DataPointParameterDouble)param).DoubleValue; }
+            if (param.GetType() == typeof(DataPointParameterInt)) { return ((DataPointParameterInt)param).IntValue; }
+            if (param.GetType() == typeof(DataPointParameterString)) { return ((DataPointParameterString)param).StringValue; }
+            if (param.GetType() == typeof(DataPointParameterSummary)) { return ((DataPointParameterSummary)param).SummaryValue; }
+
+            return "DataPointParameterString";
         }
 
         public async Task<int> GetDataPointsCount(DataPointSearchCriteria criteria, WorldInfo info)
@@ -100,7 +170,7 @@ namespace SardCoreAPI.DataAccess.DataPoints
             {
                 for (int i = 0; i < Math.Min(criteria.Parameters.Count(), 20); i++)
                 {
-                    if (criteria.Parameters[i].GetType() == typeof(DataPointParameterBoolean))
+                    /*if (criteria.Parameters[i].GetType() == typeof(DataPointParameterBoolean))
                     {
                         criteria.SetParam(i, ((DataPointParameterBoolean)criteria.Parameters[i]).BoolValue ? "1" : "0");
                         builder.LeftJoin($"DataPointParameterBoolean p{i} ON p{i}.DataPointId = dp.Id");
@@ -135,7 +205,7 @@ namespace SardCoreAPI.DataAccess.DataPoints
                         criteria.SetParam(i, "%" + ((DataPointParameterSummary)criteria.Parameters[i]).SummaryValue + "%");
                         builder.LeftJoin($"DataPointParameterSummary p{i} ON p{i}.DataPointId = dp.Id");
                         builder.Where($"p{i}.Value LIKE @Param{i}");
-                    }
+                    }*/
                 }
             }
 
---
+++ Models/DataPoints/ParameterSearchOptions.cs
@@ -0,0 +1,21 @@
+﻿using System.Text.Json.Serialization;
+
+namespace SardCoreAPI.Models.DataPoints
+{
+    public class ParameterSearchOptions
+    {
+        public int DataPointTypeParameterId { get; set; }
+        public FilterModeEnum FilterMode { get; set; }
+        [JsonIgnore]
+        public int SequenceId { get; set; }
+        public enum FilterModeEnum
+        {
+            Equals = 0,
+            Contains = 1,
+            StartsWith = 2,
+            EndsWith = 3,
+            GreaterThan = 5,
+            LessThan = 6
+        }
+    }
+}
